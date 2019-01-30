75488158e4c77de7749b8c2b113ad07393fd7f17
Sergey Neupokoev <Sergey@Sergeys-MacBook-Pro.local>
2019-01-30 22:26:20 +0400

    New Xcode project

---
+++ window-shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Sergey Neupokoev on 30/1/19.
+//  Copyright © 2019 Sergey Neupokoev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
